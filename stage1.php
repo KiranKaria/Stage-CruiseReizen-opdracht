354a2d77659afbf70f55755da2384cb4726179a4
Kiran Karia <302153553@student.rocmondriaan.nl>
2020-02-24 15:26:58 +0100

    api gemaakt

---
+++ stage1.php
@@ -0,0 +1,58 @@
+<html>
+<Title> Wiki Api2</Title>
+<body>
+<form action="stage.php" method="post">
+    Search the wiki: <input type="text" name="name">
+
+</form>
+<div class="w3-container">
+
+
+    <?php
+    $name = $_POST["name"];
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=.$name.");
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $output = curl_exec($ch);
+
+    curl_close($ch);
+
+    $var = json_decode($output);
+
+    $results = $var->query->search;
+
+
+    foreach ($results as $result) {
+
+        echo '<br>';
+        echo'<b>'.$result->title.'</b>';
+
+        echo '<br>';
+        echo '<br>';
+        echo ($result->snippet);
+        echo '<br>';
+        echo '<br>';
+        echo ($result->timestamp);
+        echo '<br>';
+        echo '<br>';
+
+    }
+
+    ?>
+
+</div>
+</body>
+</html>
+<style>
+    body{
+        background-color: darkgreen;
+        text-align:left;
+    }
+    .w3-container{
+        background-color: white;
+    }
+</style>
